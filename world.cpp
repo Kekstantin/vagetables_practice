7a9e087d39aa1276d44026b685a82c8f2b090b04
aylhttd <50331667+aylhttd@users.noreply.github.com>
2019-07-01 17:33:02 +0300

    Update world.cpp

--- world.cpp
+++ world.cpp
@@ -73,3 +73,42 @@ void world::moveAllBot() const
 	for (auto obj : collect_of_dynamic_entitys)
 		obj.second->move(this);
 }
+
+entity* world::find_target(bot* _bot)
+{
+	float distance = sqrt((this->get_collect_of_static_entitys().begin().first().first() - _bot->getX()) *
+		(this->get_collect_of_static_entitys().begin().first().first() - _bot->getX()) +
+		(this->get_collect_of_static_entitys().begin().first().second() - _bot->getY()) *
+		(this->get_collect_of_static_entitys().begin().first().second() - _bot->getY()));
+	float new_distance;
+	auto target = this->get_collect_of_static_entitys().begin().second();
+	for (auto i : this->get_collect_of_static_entitys())
+	{
+		new_distance = distance;
+		if ((_bot->getWidthAndHeight() / i.second()->getWidthAndHeight()) > 1.2) {
+			new_distance = sqrt((i.begin().first().first() - _bot->getX()) *
+				(i.begin().first().first() - _bot->getX()) +
+				(i.begin().first().second() - _bot->getY()) *
+				(i.begin().first().second() - _bot->getY()));
+		}
+		if (new_distance < distance) {
+			distance = new_distance;
+			target = i.second;
+		}
+	}
+	for (auto i : this->get_collect_of_dynamic_entitys())
+	{
+		new_distance = distance;
+		if ((_bot->getWidthAndHeight() / i.second()->getWidthAndHeight()) > 1.2) {
+			new_distance = sqrt((i.begin().first().first() - _bot->getX()) *
+				(i.begin().first().first() - _bot->getX()) +
+				(i.begin().first().second() - _bot->getY()) *
+				(i.begin().first().second() - _bot->getY()));
+		}
+		if (new_distance < distance) {
+			distance = new_distance;
+			target = i.second;
+		}
+	}
+	return target;
+}
