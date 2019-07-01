096e2b3f7a8a06fa74995535e9251d84f088667a
Vadim-ix <52409728+Vadim-ix@users.noreply.github.com>
2019-07-01 16:45:08 +0300

    Create static_entity.cpp

---
+++ static_entity.cpp
@@ -0,0 +1,11 @@
+#include "static_entity.h"
+
+static_entity::static_entity(const float widthAndHeight):entity(x,y)
+{
+	this->widthAndHeight = widthAndHeight;
+}
+
+float static_entity::getWidthAndHeight() const
+{
+	return widthAndHeight;
+}
